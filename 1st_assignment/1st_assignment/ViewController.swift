2faf0ed49457eb69572f922784a8ab5d3d9e64ff
kasiap19 <kasiap19@gmail.com>
2016-11-27 18:25:09 +0100

    adding files

---
+++ 1st_assignment/1st_assignment/ViewController.swift
@@ -0,0 +1,116 @@
+//
+//  ViewController.swift
+//  1st_assignment
+//
+//  Created by kasia on 27/11/16.
+//  Copyright © 2016 kasia. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController,UISearchBarDelegate, CLLocationManagerDelegate {
+    var LocationManager = CLLocationManager()
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    // search
+    @IBAction func showSearchbar(_ sender: AnyObject) {
+   
+   
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.hidesNavigationBarDuringPresentation = false
+        self.searchController.searchBar.delegate = self
+        present(searchController, animated: true, completion: nil)
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.hidesNavigationBarDuringPresentation = false
+        self.searchController.searchBar.delegate = self
+        present(searchController, animated: true, completion: nil)
+    
+     }
+    
+    var searchController:UISearchController!
+    var annotation:MKAnnotation!
+    var localSearchRequest:MKLocalSearchRequest!
+    var localSearch:MKLocalSearch!
+    var localSearchResponse:MKLocalSearchResponse!
+    var error:NSError!
+    var pointAnnotation:MKPointAnnotation!
+    var pinAnnotationView:MKPinAnnotationView!
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar){
+        //1
+        searchBar.resignFirstResponder()
+        dismiss(animated: true, completion: nil)
+        if self.mapView.annotations.count != 0{
+            annotation = self.mapView.annotations[0]
+            self.mapView.removeAnnotation(annotation)
+            
+        }
+        //2
+        localSearchRequest = MKLocalSearchRequest()
+        localSearchRequest.naturalLanguageQuery = searchBar.text
+        localSearch = MKLocalSearch(request: localSearchRequest)
+        localSearch.start { (localSearchResponse, error) -> Void in
+            
+            if localSearchResponse == nil{
+                let alertController = UIAlertController(title: nil, message: "Place Not Found", preferredStyle: UIAlertControllerStyle.alert)
+                alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default, handler: nil))
+                self.present(alertController, animated: true, completion: nil)
+                return
+            }
+            //3
+            self.pointAnnotation = MKPointAnnotation()
+            self.pointAnnotation.title = searchBar.text
+            self.pointAnnotation.coordinate = CLLocationCoordinate2D(latitude: localSearchResponse!.boundingRegion.center.latitude, longitude:     localSearchResponse!.boundingRegion.center.longitude)
+            
+            
+            self.pinAnnotationView = MKPinAnnotationView(annotation: self.pointAnnotation, reuseIdentifier: nil)
+            self.mapView.centerCoordinate = self.pointAnnotation.coordinate
+            self.mapView.addAnnotation(self.pinAnnotationView.annotation!)
+            
+            // display name of the city in the label
+//            self.pointedDestination.text = searchBar.text!
+        }
+        
+        
+        
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    
+            LocationManager = CLLocationManager()
+            LocationManager.delegate = self
+            LocationManager.desiredAccuracy = kCLLocationAccuracyBest
+            LocationManager.requestAlwaysAuthorization()
+            LocationManager.startUpdatingLocation()
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [AnyObject]!) {
+        let location  = locations.last as! CLLocation!
+        
+        let center = CLLocationCoordinate2DMake((location?.coordinate.latitude)!, (location?.coordinate.longitude)!)
+        
+        let region = MKCoordinateRegionMake(center, MKCoordinateSpanMake(1, 1))
+        
+        self.mapView.setRegion(region, animated: true)
+        self.LocationManager.stopUpdatingLocation()
+
+    }
+
+
+
+}
+
